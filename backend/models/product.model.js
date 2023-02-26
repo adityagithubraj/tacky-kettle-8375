5745042cd5581631e640a7a8b926768e911db9ed
adityagithubraj <adityaraj9843@gmail.com>
2023-02-26 10:30:45 +0530

    day-5

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -2,10 +2,12 @@ const mongoose=require("mongoose")
 
 const productschema=mongoose.Schema({
     title:String,
-    content:String,
-    author: String
+    content: String,
+    author:String
+    
 },{versionkey:false})
 
 const productmodel=mongoose.model("product",productschema)
 
 module.exports={productmodel}
+
